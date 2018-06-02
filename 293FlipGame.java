21d70d1a43dbe740260c3824037dfbf71de588f3
KCHEN404 <kunyuc@utexas.edu>
2018-06-02 16:06:36 -0500

    293 accepted

---
+++ 293FlipGame.java
@@ -0,0 +1,40 @@
+/**
+ * 293 Flip Game
+ * Related Topics: String
+ */
+
+ /**
+  * My accepted version
+  * Time Complexity: O(n^2)
+  * Space Complexity: O(n)
+  *
+  * Error Prone:
+  * 1. substring(beginning index, ending index): ending index
+  *    is the index of the first char that is not included
+  * 2. List<Type> list_name = new ArrayList<>();
+  * 3. list.add();
+  * 4. StringBuilder name = new StringBuilder();
+  * 5. stringbuilder.append();
+  * 6. substring is O(n)
+  */
+
+class Solution {
+    public List<String> generatePossibleNextMoves(String s) {
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < s.length() - 1; i++) {
+            String sub = s.substring(i, i+2);
+            if (sub.equals("++")) {
+                StringBuilder res = new StringBuilder();
+                if (i > 0) {
+                    res.append(s.substring(0, i));
+                }
+                res.append("--");
+                if (i + 1 < s.length() - 1) {
+                    res.append(s.substring(i + 2, s.length()));
+                }
+                result.add(res.toString());
+            }
+        }
+        return result;
+    }
+}
--- TODO.md
+++ TODO.md
@@ -1,2 +1,1 @@
 # TODO
-Review array and hashing
