35462da35085e28470067ac360103812e95043c5
KCHEN404 <kunyuc@utexas.edu>
2018-06-03 16:19:06 -0500

    update todo and iterative 226

--- 226InvertBinaryTree.java
+++ 226InvertBinaryTree.java
@@ -4,18 +4,15 @@
  */
 
  /**
-  * version 1
+  * recusive version
   * Time Complexity: O(n)
-  * Space Complexity: O(1)
+  * Space Complexity: O(n)
   *
   * Error Prone:
-  * 1. substring(beginning index, ending index): ending index
-  *    is the index of the first char that is not included
-  * 2. List<Type> list_name = new ArrayList<>();
-  * 3. list.add();
-  * 4. StringBuilder name = new StringBuilder();
-  * 5. stringbuilder.append();
-  * 6. substring is O(n)
+  * 1. Because of recursion, O(h) function calls
+  * will be placed on the stack in the worst case,
+  * where h is the height of the tree. So, the 
+  * space complexity is O(n).
   */
 
 class Solution {
@@ -27,6 +24,44 @@ class Solution {
         TreeNode right = invertTree(root.right);
         root.left = right;
         root.right = left;
+        return root;
+    }
+}
+
+/**
+ * Iterative version
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ *
+ * Error Prone:
+ * 1. Queue is an interface (can't be instantiated directly)
+ *    need to create an instance of a class implementing
+ *    that interface
+ * 2. Queue<Type> queue = new LinkedList<Type>();
+ * 3. queue.add();
+ * 4. queue.poll();
+ * 5. queue.isEmpty();
+ */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            TreeNode tmp = current.left;
+            current.left = current.right;
+            current.right = tmp;
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
         return root;
     }
 }
--- TODO.md
+++ TODO.md
@@ -1,1 +1,2 @@
 # TODO
+Review Tree and Interface
