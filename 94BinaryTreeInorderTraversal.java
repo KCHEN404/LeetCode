5e5745076dcab1c439152bad9e214d29b9d37136
KCHEN404 <kunyuc@utexas.edu>
2018-07-05 21:37:39 -0500

    94 recursion accepted

--- 94BinaryTreeInorderTraversal.java
+++ 94BinaryTreeInorderTraversal.java
@@ -7,15 +7,25 @@
   * My accepted version
   *  
   * Time Complexity: O(n)
-  * Space Complexity: O(1)
+  * Space Complexity: O(n)
+  * 
+  * Basic idea: recursion
   *
   */
 
   class Solution {
-    public int maxDepth(TreeNode root) {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root,res);
+        return res;
+    }
+    
+    private void inorder(TreeNode root, List<Integer> res) {
         if (root == null) {
-            return 0;
+            return;
         }
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
     }
 }
