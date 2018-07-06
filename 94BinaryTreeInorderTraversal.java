66e825a3947a5a107e025268d2f66e466eb4f61e
KCHEN404 <kunyuc@utexas.edu>
2018-07-06 11:04:30 -0500

    94 iterative accepted

--- 94BinaryTreeInorderTraversal.java
+++ 94BinaryTreeInorderTraversal.java
@@ -4,12 +4,10 @@
  */
 
  /**
-  * My accepted version
+  * Recursive version
   *  
   * Time Complexity: O(n)
   * Space Complexity: O(n)
-  * 
-  * Basic idea: recursion
   *
   */
 
@@ -27,5 +25,43 @@
         inorder(root.left, res);
         res.add(root.val);
         inorder(root.right, res);
+    }
+}
+
+ /**
+  * Iterative version
+  *  
+  * Time Complexity: O(n)
+  * Space Complexity: O(n)
+  * 
+  * Error Prone:
+  * 1. stack class in java
+  * 2. stack.push()
+  * 3. stack.pop()
+  * 4. stack.empty()
+  * 5. stack.peek()
+  * 6. stack.search()
+  *
+  */
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stk = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stk.empty()) {
+            if (cur != null) {
+                stk.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stk.pop();
+                res.add(cur.val);
+                cur = cur.right;
+            }
+        }
+        return res;
     }
 }
